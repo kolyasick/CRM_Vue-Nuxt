4cf3248692592af2a7906b37071c696dc18ff80f
Nikolaj <154469630+kolyasick@users.noreply.github.com>
2024-05-29 21:58:36 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,71 +1,49 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-  modules: [
-    '@nuxt/ui',
-    '@nuxt/image',
-    'shadcn-nuxt',
-    'nuxt-icon',
-    '@nuxtjs/i18n',
-    '@pinia/nuxt',
-    [
-      '@vee-validate/nuxt',
-      {
-        autoImports: true,
-      },
+  devtools: { enabled: true },
+    modules: [
+        '@nuxt/ui',
+        '@nuxt/image',
+        'shadcn-nuxt',
+        'nuxt-icon',
+        '@nuxtjs/i18n',
+        '@pinia/nuxt',
+        [
+            '@vee-validate/nuxt',
+            {
+                autoImports: true,
+            },
+        ],
+        ['@nuxtjs/google-fonts', {
+            families: {
+                Lato: {
+                    wght: [300, 400, 700],
+                    ital: [300]
+                }
+            }
+        }],
     ],
-    ['@nuxtjs/google-fonts', {
-      families: {
-        Lato: {
-          wght: [300, 400, 700],
-          ital: [300]
-        }
-      }
-    }],
-  ],
-  shadcn: {
-    prefix: 'Ui',
-    componentDir: './components/ui'
-  },
+    shadcn: {
+        prefix: 'Ui',
+        componentDir: './components/ui'
+    },
 
-  i18n: {
-    langDir: 'i18n',
-    defaultLocale: 'en',
-    trailingSlash: true,
-    detectBrowserLanguage: false,
-    locales: [
-      { code: 'en', file: 'en.json', name: 'Eng' },
-      { code: 'uk', file: 'uk.json', name: 'Укр' },
-    ],
-    compilation: {
-      strictMessage: false,
+    i18n: {
+        langDir: 'i18n',
+        defaultLocale: 'en',
+        trailingSlash: true,
+        detectBrowserLanguage: false,
+        locales: [
+            { code: 'en', file: 'en.json', name: 'Eng' },
+            { code: 'uk', file: 'uk.json', name: 'Укр' },
+        ],
+        compilation: {
+            strictMessage: false,
+        },
     },
-  },
-  pinia: {
-    storesDirs: ['./store/**']
-  },
-  plugins: ['@/plugins/chart.js'],
-  buildModules: [
-    '@pinia/nuxt',
-  ],
-  nitro: {
-    prerender: {
-      routes: [
-        '/', // добавьте сюда все маршруты, которые нужно пререндерить
-        '/feedback',
-        '/help',
-        '/orders',
-        '/payments',
-        '/settings',
-        '/stat',
-        '/stats',
-        '/customers',
-        '/Login'
-      ],
+    pinia: {
+      storesDirs: ['./store/**']
     },
-  },
-  generate: {
-    fallback: true, // генерирует 404.html для всех несуществующих страниц
-  },
-}
+    plugins: ['@/plugins/chart.js'],
 })
+
