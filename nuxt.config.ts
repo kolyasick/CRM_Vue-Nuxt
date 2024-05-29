96e843fc06beb4802a6a96e15c61c073088eb9c9
Nikolaj <154469630+kolyasick@users.noreply.github.com>
2024-05-29 21:51:57 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,53 +1,60 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-    modules: [
-        '@nuxt/ui',
-        '@nuxt/image',
-        'shadcn-nuxt',
-        'nuxt-icon',
-        '@nuxtjs/i18n',
-        '@pinia/nuxt',
-        [
-            '@vee-validate/nuxt',
-            {
-                autoImports: true,
-            },
-        ],
-        ['@nuxtjs/google-fonts', {
-            families: {
-                Lato: {
-                    wght: [300, 400, 700],
-                    ital: [300]
-                }
-            }
-        }],
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/image',
+    'shadcn-nuxt',
+    'nuxt-icon',
+    '@nuxtjs/i18n',
+    '@pinia/nuxt',
+    [
+      '@vee-validate/nuxt',
+      {
+        autoImports: true,
+      },
     ],
-    shadcn: {
-        prefix: 'Ui',
-        componentDir: './components/ui'
-    },
+    ['@nuxtjs/google-fonts', {
+      families: {
+        Lato: {
+          wght: [300, 400, 700],
+          ital: [300]
+        }
+      }
+    }],
+  ],
+  shadcn: {
+    prefix: 'Ui',
+    componentDir: './components/ui'
+  },
 
-    i18n: {
-        langDir: 'i18n',
-        defaultLocale: 'en',
-        trailingSlash: true,
-        detectBrowserLanguage: false,
-        locales: [
-            { code: 'en', file: 'en.json', name: 'Eng' },
-            { code: 'uk', file: 'uk.json', name: 'Укр' },
-        ],
-        compilation: {
-            strictMessage: false,
-        },
+  i18n: {
+    langDir: 'i18n',
+    defaultLocale: 'en',
+    trailingSlash: true,
+    detectBrowserLanguage: false,
+    locales: [
+      { code: 'en', file: 'en.json', name: 'Eng' },
+      { code: 'uk', file: 'uk.json', name: 'Укр' },
+    ],
+    compilation: {
+      strictMessage: false,
     },
-    pinia: {
-      storesDirs: ['./store/**']
-    },
-    plugins: ['@/plugins/chart.js'],
-    target: 'static', // default is 'server'
-    generate: {
-      fallback: true
-    },
+  },
+  pinia: {
+    storesDirs: ['./store/**']
+  },
+  plugins: ['@/plugins/chart.js'],
+  target: 'static', // This is correct for static sites
+  router: {
+    base: '/CRMveuNuxt/' // Make sure this matches your GitHub Pages repo name
+  },
+  generate: {
+    fallback: true, // This allows your app to handle 404s
+  },
+  head: {
+    link: [
+      { rel: 'preload', href: '/CRMveuNuxt/_payload.json', as: 'fetch', type: 'application/json', crossorigin: 'use-credentials' }
+    ]
+  }
 })
-
