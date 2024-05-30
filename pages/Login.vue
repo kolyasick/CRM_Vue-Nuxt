880c9e0865266d1dd20a5fb5bda4024fd39a6575
kolyasick <vana63393@gmail.com>
2024-05-30 20:40:49 +0300

    validation

--- pages/Login.vue
+++ pages/Login.vue
@@ -1,7 +1,7 @@
-<script setup>
+<script setup lang="ts">
 
-import {useIsLoadingStore} from "~/store/auth.ts";
-import {useAuthStore} from "~/store/auth.ts";
+import {useIsLoadingStore} from "~/store/auth";
+import {useAuthStore} from "~/store/auth";
 import {v4 as uuid} from 'uuid';
 
 const emailRef = ref('')
@@ -11,26 +11,61 @@ const nameRef = ref('')
 const router = useRouter();
 const isLoadingStore = useIsLoadingStore();
 const authStore = useAuthStore();
+const errors = ref<string | null>(null)
+
+const emailRegex = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+const passwordRegex = '^.{8,}$'
 
 const login = async () => {
-  isLoadingStore.set(true)
-  await account.createEmailSession(emailRef.value, passwordRef.value)
-  const response = await account.get();
-  if (response) {
-    authStore.set({
-      email: response.email,
-      name: response.name,
-      status: response.status
-  })
-    emailRef.value = ''
-    passwordRef.value = ''
-    nameRef.value = ''
 
-    await router.push('/')
+  if (!emailRef.value || !passwordRef.value) {
+    errors.value = 'Please enter email and password'
+    return
+  } else if (!emailRef.value.match(emailRegex)) {
+    errors.value = 'Please enter valid email'
+    return
+  } else if (!passwordRef.value.match(passwordRegex)) {
+    errors.value = 'Password must be at least 8 characters'
+    return
+  }
+
+
+  try {
+    await account.createEmailSession(emailRef.value, passwordRef.value)
+    const response = await account.get()
+    if (response) {
+      isLoadingStore.set(true)
+      authStore.set({
+        email: response.email,
+        name: response.name,
+        status: response.status
+      })
+      emailRef.value = ''
+      passwordRef.value = ''
+      nameRef.value = ''
+      await router.push('/')
+    }
+  } catch (error) {
+    errors.value = 'Invalid email or password'
+  } finally {
     isLoadingStore.set(false)
-}}
+  }
+}
+
 
 const register = async () => {
+  errors.value = null
+
+  if (!emailRef.value || !passwordRef.value) {
+    errors.value = 'Please enter email and password'
+    return
+  } else if (!emailRef.value.match(emailRegex)) {
+    errors.value = 'Please enter valid email'
+    return
+  } else if (!passwordRef.value.match(passwordRegex)) {
+    errors.value = 'Password must be at least 6 characters'
+    return
+  }
   await account.create(uuid(), emailRef.value, passwordRef.value, nameRef.value)
   await login();
 }
@@ -47,9 +82,15 @@ useHead({
         <h1 class="font-bold text-2xl mb-5 text-center">Login</h1>
 
         <form>
-            <UiInput v-model="emailRef" placeholder="Email" type="email"  class="mb-3" />
-            <UiInput v-model="passwordRef" placeholder="Password" type="password"  class="mb-3" />
-            <UiInput v-model="nameRef" placeholder="Name" type="name"  class="mb-3" />
+            <UiInput @input="errors=null" v-model="emailRef" placeholder="Email" type="email"  class="mb-3" />
+            <UiInput @input="errors=null" v-model="passwordRef" placeholder="Password" type="password"  class="mb-3" />
+            <UiInput @input="errors=null" v-model="nameRef" placeholder="Name" type="name"  class="mb-3" />
+
+            <span v-if="errors">
+                <p class="text-red-800">
+                  * {{errors}}
+                </p>
+            </span>
 
           <div class="flex items-center gap-3 justify-start mt-4 flex-wrap">
               <UiButton @click="login" variant="ghost" type="button">Login</UiButton>
