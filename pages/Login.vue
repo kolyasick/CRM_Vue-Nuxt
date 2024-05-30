caf87d1f2b7a3ed6be15092b829c59a6614a2fee
Nikolaj <154469630+kolyasick@users.noreply.github.com>
2024-05-30 20:56:49 +0300

    validation

--- pages/Login.vue
+++ pages/Login.vue
@@ -13,7 +13,7 @@ const isLoadingStore = useIsLoadingStore();
 const authStore = useAuthStore();
 const errors = ref<string | null>(null)
 
-const emailRegex = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 const passwordRegex = '^.{8,}$'
 
 const login = async () => {
